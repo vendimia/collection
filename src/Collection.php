6ae9fdabb75d397021bb6571ab02e53bf816dced
Oliver Etchebarne <yo@drmad.org>
2021-09-08 10:57:29 -0500

    Initial commit

---
+++ src/Collection.php
@@ -0,0 +1,238 @@
+<?php
+namespace Vendimia\Collection;
+
+use Vendimia\Interface\DataType\Arrayable;
+use ArrayAccess;
+use Countable;
+use Iterator;
+
+/**
+ * Collection: Object-oriented array reimplementation
+ */
+class Collection implements Arrayable, ArrayAccess, Countable, Iterator
+{
+    protected $storage = [];
+
+    public function __construct(array|Arrayable $array) {
+        if ($array instanceof Arrayable) {
+            $this->storage = $array->asArray();
+        } else {
+            $this->storage = $array;
+        }
+    }
+
+    /**
+     * Returns true when this collection is a list.
+     *
+     * A list has sequential positive integer indexes starting with 0
+     */
+    public function isList(): bool
+    {
+        return array_is_list($this->storage);
+    }
+
+    /**
+     * Returns whether an index exists on the collection
+     */
+    public function has($index): bool
+    {
+        return key_exists($index, $this->storage);
+    }
+
+    /**
+     * Returns an element of this collection.
+     */
+    public function &get($index): mixed
+    {
+        return $this->storage[$index];
+    }
+
+    /**
+     * Searches for a value in the collection, returns the index or key, or
+     * false if the value doesn't exists.
+     */
+    public function search($value): int|string|false
+    {
+        return array_search($this->storage, $value);
+    }
+
+    /**
+     * Adds or update an element to this collection.
+     *
+     * If the collection is a list, omitting the offset will append the value.
+     */
+    public function set($value, $offset = ''): self
+    {
+        if (array_is_list($this->storage) && !$offset) {
+            $this->storage[] = $value;
+        } else {
+            $this->storage[$offset] = $value;
+        }
+        return $this;
+    }
+
+    /**
+     * Returns the elements count.
+     */
+    public function length(): int
+    {
+        return count($this->storage);
+    }
+
+    /**
+     * Appends elements to the collection.
+     */
+    public function append(...$elements): self
+    {
+        $this->storage = [...$this->storage, ...$elements];
+        return $this;
+    }
+
+    /**
+     * Prepends elements to the collection.
+     */
+    public function prepend(...$elements): self
+    {
+        $this->storage = [...$elements, ...$this->storage];
+        return $this;
+    }
+
+    /**
+     * Removes an element with index $index.
+     *
+     * On lists, this rearrange the numeric indexes.
+     */
+    public function remove($index)
+    {
+        // Si el array está vacío, no hacemos nada
+        if (!$this->storage) {
+            return;
+        }
+
+        if (array_is_list($this->storage)) {
+            array_splice($this->storage, $index, 1);
+        } else {
+            unset($this->storage[$index]);
+        }
+    }
+
+    /**
+     * Filters elements and returns a new collection
+     */
+    public function filter(?Callable $callback = null): self
+    {
+        return new self(array_filter($this->storage, $callback));
+    }
+
+    /**
+     * Filters element's keys and returns a new collection
+     */
+    public function filterKey(?Callable $callback = null): self
+    {
+        return new self(array_filter($this->storage, $callback, ARRAY_FILTER_USE_KEY));
+    }
+
+    /**
+     * Returns a collection with every element passed via a callback
+     */
+    public function map(Callable $callback): self
+    {
+        return new self(array_map($callback, $this->storage));
+    }
+
+    /**
+     * Arrayable implementation
+     */
+    public function asArray(): array
+    {
+        return $this->storage;
+    }
+
+    /**
+     * Returns the array when called via var_dump() and friends.
+     */
+    public function __debugInfo()
+    {
+        return $this->storage;
+    }
+
+    /**
+     * ArrayAccess::offsetExists — Whether an offset exists
+     */
+    public function offsetExists(mixed $offset): bool
+    {
+        return key_exists($offset, $this->storage);
+    }
+
+    /**
+     * ArrayAccess::offsetGet — Offset to retrieve
+     */
+    public function &offsetGet(mixed $offset): mixed
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * ArrayAccess::offsetSet — Assign a value to the specified offset
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->set($value, $offset);
+    }
+
+    /**
+     * ArrayAccess::offsetUnset — Unset an offset
+     */
+    public function offsetUnset(mixed $offset): void
+    {
+        $this->remove($offset);
+    }
+
+    /**
+     * Countable::count — Count elements of an object
+     */
+    public function count(): int
+    {
+        return $this->length();
+    }
+
+    /**
+     * Iterator::current — Return the current element
+     */
+    public function current(): mixed
+    {
+        return current($this->storage);
+    }
+
+    /**
+     * Iterator::key — Return the key of the current element
+     */
+    public function key(): mixed
+    {
+        return key($this->storage);
+    }
+
+    /**
+     * Iterator::next — Move forward to next element
+     */
+    public function next(): void
+    {
+        next($this->storage);
+    }
+
+    /**
+     * Iterator::rewind — Rewind the Iterator to the first element
+     */
+    public function rewind(): void
+    {
+        reset($this->storage);
+    }
+
+    /**
+     * Iterator::valid — Checks if current position is valid
+     */
+    public function valid(): bool
+    {
+        return current($this->storage) !== false;
+    }
+}
---
+++ tests/CollectionTest.php
@@ -0,0 +1,166 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use Vendimia\Collection\Collection;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+final class CollectionTest extends TestCase
+{
+    public function testInitializeCollection(): Collection
+    {
+        $test = new Collection(['orange', 'apple', 'banana']);
+
+        $this->assertEquals($test->asArray(), ['orange', 'apple', 'banana']);
+
+        return $test;
+    }
+
+    public function testInitializeCollectionWithDict(): Collection
+    {
+        $test = [
+            'name' => 'Oliver',
+            'age' => '40',
+            'address' => '123 Testing St.'
+        ];
+
+        $collection = new Collection($test);
+
+        $this->assertEquals(
+            $test,
+            $collection->asArray()
+        );
+
+        return $collection;
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testLength(Collection $collection)
+    {
+        $this->assertEquals($collection->length(), 3);
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testCount(Collection $collection)
+    {
+        $this->assertEquals(count($collection), count($collection->asArray()));
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testAppend(Collection $collection)
+    {
+        $collection->append('dragon fruit', 'carambola');
+
+        $this->assertEquals($collection->asArray(), [
+            'orange', 'apple', 'banana', 'dragon fruit', 'carambola'
+        ]);
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testPrepend(Collection $collection)
+    {
+        $collection->prepend('pear', 'peach');
+
+        $this->assertEquals($collection->asArray(), [
+            'pear', 'peach', 'orange', 'apple', 'banana', 'dragon fruit', 'carambola'
+        ]);
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testRemoveFromList(Collection $collection)
+    {
+        $collection->remove(2);
+
+        $this->assertEquals($collection->asArray(), [
+            'pear', 'peach', 'apple', 'banana', 'dragon fruit', 'carambola'
+        ]);
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testGet(Collection $collection)
+    {
+        $this->assertEquals($collection->get(2), 'apple');
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testGetWithSquareBrackets(Collection $collection)
+    {
+        $this->assertEquals('apple', $collection[2]);
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testIterateElements(Collection $collection)
+    {
+        $result = [];
+        foreach ($collection as $element) {
+            $result[] = $element;
+        }
+
+        $this->assertEquals(
+            ['pear', 'peach', 'apple', 'banana', 'dragon fruit', 'carambola'],
+            $result
+        );
+    }
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testAddViaSquareBrakets(Collection $collection)
+    {
+        $collection[] = 'strawberry'; // I know...
+
+        $this->assertEquals([
+            'pear', 'peach', 'apple', 'banana', 'dragon fruit', 'carambola',
+            'strawberry',
+        ], $collection->asArray());
+    }
+
+
+    /**
+     * @depends testInitializeCollection
+     */
+    public function testFilterValues(Collection $collection)
+    {
+        $this->assertEquals(
+            ['pear', 'peach'],
+            $collection->filter(fn($x) => str_starts_with($x, 'p'))->asArray()
+        );
+    }
+
+    /**
+     * @depends testInitializeCollectionWithDict
+     */
+    public function testFilterKeys(Collection $collection)
+    {
+        $this->assertEquals(
+            ['age' => 40, 'address' => '123 Testing St.'],
+            $collection->filterKey(fn($x) => str_starts_with($x, 'a'))->asArray()
+        );
+    }
+
+
+    public function testMap()
+    {
+        $numbers = new Collection([2, 4, 6, 8]);
+
+        $this->assertEquals(
+            [4, 16, 36, 64],
+            $numbers->map(fn($x) => $x * $x)->asArray(),
+        );
+    }
+}
